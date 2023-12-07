266a564d52e08ea55c28dcb09ea3ff8dc57620af
Thiago Franke Melchiors <105611304+TFrankeM@users.noreply.github.com>
2023-12-07 14:09:26 -0300

    Adicione as classes produto e vendedor

--- structure.cpp
+++ structure.cpp
@@ -1,5 +1,5 @@
 #include "structure.h"
-
+#include <algorithm> // Para find_if e remove_if
 
 // Construtor da classe EdgeNode
 EdgeNode::EdgeNode(vertex m_otherVertex, EdgeNode* m_next, int m_length)
@@ -111,4 +111,72 @@ void GraphAdjList::print()
         }
         cout << endl;
     }
+}
+
+// ==============================================
+
+
+// Constructor da classe produto
+Product::Product(const string& prodId, double prodPrice, double prodWeight)
+    : id(prodId)
+    , price(prodPrice)
+    , weight(prodWeight) 
+{}
+
+// retorna o produto ID
+std::string Product::getId() const { return id; }
+
+// Retorna o preço do produto
+double Product::getPrice() const { return price; }
+
+// Retorna o peso do produto
+double Product::getWeight() const { return weight; }
+
+// Atribui o preço do produto
+void Product::setPrice(double newPrice) { price = newPrice; }
+
+// Atribui o peso do produto
+void Product::setWeight(double newWeight) { weight = newWeight; }
+
+// ==============================================
+
+// Construtor da classe vendedor
+Seller::Seller(const string& sellerId)
+    : id(sellerId)
+{}
+
+// Retorna o ID do vendedor
+string Seller::getId() const { return id; }
+
+// Adiciona novo produto ao catálogo do vendedor
+void Seller::addProduct(const Product& product) {
+    catalog.push_back(product);
+}
+
+// Remove um produto do catálogo do vendedor pelo seu ID
+void Seller::removeProduct(const string& productId) {
+    // O algoritmo remove_if reorganiza os elementos no intervalo [begin, end)
+    // e retorna um iterador para o novo final desse intervalo.
+    // Aqui, ele remove o produto cujo ID corresponde ao productId especificado.
+    catalog.erase(remove_if(catalog.begin(), catalog.end(),
+                                [&productId](const Product& product) {
+                                    return product.getId() == productId;
+                                }),
+                            catalog.end());
+}
+
+// Recupera um produto do catálogo do vendedor pelo seu ID
+Product Seller::getProduct(const string& productId) const {
+    // O algoritmo find_if procura por um elemento para o qual o fornecido
+    // predicado retorna verdadeiro e retorna um iterador para ele.
+    // Aqui, procura um produto com o productId especificado.
+    // A função lambda verifica se o ID do produto corresponde ao productId especificado.
+    auto it = find_if(catalog.begin(), catalog.end(),
+                                [&productId](const Product& product) {
+                                    return product.getId() == productId;
+                                });
+    if (it != catalog.end()) {
+        return *it;
+    }
+    throw runtime_error("Product not found");
 }
--- structure.h
+++ structure.h
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 // Cria o tipo "vertex" que é um inteiro
 typedef int vertex;
+
 
 /* EdgeNode Header*/
 class EdgeNode
@@ -21,6 +23,7 @@ public:
     void setNext(EdgeNode* next); // Atribui o próximo nó
 };
 
+
 /* GraphAdjList Header*/
 class GraphAdjList
 {
@@ -35,4 +38,47 @@ public:
     void addEdge(vertex v1, vertex v2, int length); // Adiciona uma aresta
     void removeEdge(vertex v1, vertex v2); // Remove uma aresta
     void print(); // Imprime o grafo
+};
+
+
+/* Product Header*/
+class Product 
+{
+private:
+    string id;          // Identificador único para product
+    double price;       // Preço do produto
+    double weight;      // Peso do produto
+
+public:
+    // Construtor para initializar um objeto Product
+    Product(const string& prodId, double prodPrice, double prodWeight);
+
+    // Métodos de acesso para retornar atributos do produto
+    string getId() const;         // Pega o product ID
+    double getPrice() const;      // Pega o product preço
+    double getWeight() const;     // Pega o product peso
+
+    // Métodos de mudança para trocar atributos do produto
+    void setPrice(double newPrice);             // Define um novo preço para o produto
+    void setWeight(double newWeight);           // Define um novo peso para o produto
+};
+
+
+/* Seller Header*/
+class Seller {
+private:
+    string id;                  // Identificador único para seller
+    vector<Product> catalog;    // Uma lista de produtos que o vendedor oferece
+
+public:
+    // Construtor para initializar um objeto Seller
+    Seller(const string& sellerId);
+
+    // Métodos de acesso para retornar atributos seller ID
+    string getId() const;
+
+    // Métodos de mudança para trocar atributos do produto
+    void addProduct(const Product& product);            // Adiciona novo produto
+    void removeProduct(const string& productId);        // Remove produto
+    Product getProduct(const string& productId) const;  // Pega produto
 };
